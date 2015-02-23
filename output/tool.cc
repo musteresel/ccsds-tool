1db5d79ccce2a3b77e9c1c44192fc3b11481961e
Daniel Jour <Daniel.Oertwig@gmail.com>
2015-02-23 11:30:57 +0100

    generation like demonstrated in testcase.cc

--- output/tool.cc
+++ output/tool.cc
@@ -25,6 +25,34 @@ struct Decl
   {}
 };
 
+
+class Declaration
+{
+  public:
+    virtual void implement(std::ostream & os) const = 0;
+};
+class StructureDecl : public Declaration
+{
+  public:
+    using fields_type = std::forward_list<Field>;
+    std::string const name;
+    fields_type const fields;
+    template<typename STR, typename FLDS>
+      StructureDecl(STR&& n, FLDS&& f)
+      : name(std::forward<STR>(n)), fields(std::forward<FLDS>(f))
+      {}
+    void implement(std::ostream & os) const
+    {
+      os << "void " << name << "::doit(void) const {";
+      for (auto field : fields)
+      {
+        os << "::doit(" << field.name << ");";
+      }
+      os << "}";
+    }
+};
+
+
 void printSignature(std::ostream & os, Decl const & decl)
 {
   os << "template<> void doit(" << decl.name << " const & v)";
@@ -51,6 +79,10 @@ int main(int argc, char ** argv)
   auto f = Field("field");
   auto d = Decl("Decl", Decl::fields_type({f, Field("more"), Field("less")}));
   print(std::cout, d);
+
+  StructureDecl("Str", StructureDecl::fields_type({f, Field("well")}))
+    .implement(std::cout);
+  std::cout << std::endl;
   return 0;
 }
 
